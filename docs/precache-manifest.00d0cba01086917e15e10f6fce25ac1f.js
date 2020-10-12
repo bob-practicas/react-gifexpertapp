aed0b9bb372f803d96075091c1548347d9ffd7ed
Roberto Ontiveros <escuela2460@gmail.com>
2020-10-12 12:19:58 -0500

    docs Agregado

---
+++ docs/precache-manifest.00d0cba01086917e15e10f6fce25ac1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725acc872028ae5a025d1ccdb27efa33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e80ab6c8cc857aa764",
+    "url": "/static/css/main.05f3514c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e80ab6c8cc857aa764",
+    "url": "/static/js/main.094b0649.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
